7e58f33bc7fc62a5ed15bc0c234a94abc6daa7e7
profbreno <brenod123@gmail.com>
2021-06-15 12:14:40 -0300

    Customização de functions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-function CardItem(){
-  return <div>Item 1</div>;
+function CardItem(props) {
+  return (
+    <div>
+      <h2>{props.nome}</h2>
+      <img src={props.img} alt={props.nome} width="300" />
+    </div>
+  );
 }
 
-function ListarItens(){
-  return <div>
-      <CardItem />
-      <CardItem />
-    </div>;
+function ListarItens() {
+  return (
+    <div>
+      <CardItem
+        nome="Golum"
+        img="https://img.olhardigital.com.br/wp-content/uploads/2019/11/20191119040151-860x450.jpg"
+      />
+      <CardItem
+        nome="Frodo"
+        img="https://rollingstone.uol.com.br/media/_versions/senhor-dos-aneis-frodo-assassino-divulgacao-netflix_widelg.jpg"
+      />
+    </div>
+  );
 }
 
-function App(){
+function App() {
   return <ListarItens />;
 }
 
@@ -21,5 +34,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

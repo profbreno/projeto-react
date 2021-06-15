5fdd9321b449879ceb560dcfb8a3c2f64484f71d
profbreno <brenod123@gmail.com>
2021-06-15 12:58:36 -0300

    Criação e configuração do CSS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 
 function CardItem(props) {
   return (
-    <div>
+    <div className="card_item">
       <h2>{props.nome}</h2>
       <img src={props.img} alt={props.nome} width="300" />
     </div>
@@ -13,7 +13,7 @@ function CardItem(props) {
 
 function ListarItens() {
   return (
-    <div>
+    <div className="lista_itens">
       <CardItem
         nome="Golum"
         img="https://img.olhardigital.com.br/wp-content/uploads/2019/11/20191119040151-860x450.jpg"
@@ -21,6 +21,10 @@ function ListarItens() {
       <CardItem
         nome="Frodo"
         img="https://rollingstone.uol.com.br/media/_versions/senhor-dos-aneis-frodo-assassino-divulgacao-netflix_widelg.jpg"
+      />
+      <CardItem
+        nome="Gandalf"
+        img="https://assets.b9.com.br/wp-content/uploads/2019/08/amazon-o-senhor-dos-aneis.jpg"
       />
     </div>
   );
